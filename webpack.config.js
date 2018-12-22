a6de4c119768dee433f9bbebe6318784effafb3a
mariomui <yi.kan.mui@gmail.com>
2018-12-22 10:39:06 -0800

    Fixed babel and jest build issue
    the @ presets conflict with jest.
    Also, a preloader that ignores node modules has to be used and enforced
    When using with transpiling loaders (like babel-loader), make sure
    they are in correct order (bottom to top).
    Otherwise files will be checked after being processed by babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,12 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   enforce: 'pre',
-      //   test: /\.jsx?$/,
-      //   loader: 'eslint-loader',
-      //   exclude: /node_modules/,
-      // },
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
