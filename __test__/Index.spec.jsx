76203a2f6ac4c064a9f57feee1dce96a51e51f83
mariomui <yi.kan.mui@gmail.com>
2018-12-22 15:26:15 -0800

    Replaced my serializer
    In order for webdevserver to work, the snapshop serializer had to be
    replaced with enzyme-to-json/serializer.

--- __test__/Index.spec.jsx
+++ __test__/Index.spec.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json'; // added this line
 import Index from '../client/Index';
+
+
+Enzyme.configure({ adapter: new Adapter() });
 
 
 test('hey', () => {
   const tree = shallow(<Index />);
-  expect(tree).toMatchSnapshot();
+  expect(toJson(tree)).toMatchSnapshot();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,34 @@
 const path = require('path');
 
-module.exports = {
-  devServer: {
-    publicPath: '/public/dist/',
-    historyApiFallback: {
-      index: './public/index.html',
-    },
-  },
-  // context: __dirname,
+const config = {
   mode: 'development',
-  entry: path.resolve(__dirname, './client/Entry.jsx'),
+  entry: path.resolve(__dirname, 'client/Entry.jsx'),
   output: {
     path: path.resolve(__dirname, 'public/dist'),
     filename: 'bundle.js',
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: true,
-  },
   module: {
     rules: [
-      // {
-      //   enforce: 'pre',
-      //   test: /\.jsx?$/,
-      //   loader: 'eslint-loader',
-      //   exclude: /node_modules/,
-      // },
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
         exclude: /node_modules/,
-        loader: 'babel-loader',
       },
     ],
   },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public/'),
+    publicPath: path.resolve(__dirname, 'public/dist'),
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
 };
+
+module.exports = config;
